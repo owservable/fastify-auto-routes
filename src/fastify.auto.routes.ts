b68006b5e4680e8103bcb549c2e2da26b8de2025
Predrag Stojadinovic <predrags@nvidia.com>
2021-12-20 23:01:13 +0100

    initial

---
+++ src/fastify.auto.routes.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+// routing
+import RoutesMap from './routes.map';
+
+import addFastifyRoutes from './functions/add.fastify.routes';
+import cleanRelativePath from './functions/clean.relative.path';
+import processFastifyBlipp from './functions/process.fastify.blipp';
+
+export default {
+	// routing
+	RoutesMap,
+	// routing functions
+	addFastifyRoutes,
+	cleanRelativePath,
+	processFastifyBlipp
+};
