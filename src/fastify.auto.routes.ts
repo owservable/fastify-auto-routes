05502a4bc739b27d262d7cd0cc399ea5af41b9c1
Predrag Stojadinovic <predrags@nvidia.com>
2022-11-26 14:00:40 +0100

    chore: adding addActionRoutes

--- src/fastify.auto.routes.ts
+++ src/fastify.auto.routes.ts
@@ -3,13 +3,16 @@
 // routing
 import RoutesMap from './routes.map';
 
+import addActionRoutes from './functions/add.action.routes';
 import addFastifyRoutes from './functions/add.fastify.routes';
 import cleanRelativePath from './functions/clean.relative.path';
 
 export {
 	// routing
 	RoutesMap,
+
 	// routing functions
+	addActionRoutes,
 	addFastifyRoutes,
 	cleanRelativePath
 };
