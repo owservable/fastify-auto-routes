e59e770f65d2869a0d4ebc5e1ecb73ebcd61168d
cope <predrag@stojadinovic.net>
2024-05-31 12:30:21 +0200

    chore: upgrading packages

--- src/functions/add.route.ts
+++ src/functions/add.route.ts
@@ -7,13 +7,13 @@ import RoutesMap from '../routes.map';
 const METHODS: string[] = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'];
 
 const addRoute = (fastify: any, route: any, relativeFilePath: string, verbose: boolean = false): void => {
-	if (!_.has(route, 'url')) route.url = '/';
+	if (!_.has(route, 'url')) _.set(route, 'url', '/');
 
 	const {url} = route;
 	if (!_.startsWith(_.toLower(url), relativeFilePath)) route.url = fixUrl(url, relativeFilePath);
 
 	if (_.has(route, 'method')) route.method = _.toUpper(route.method);
-	else route.method = 'GET';
+	else _.set(route, 'method', 'GET');
 
 	if (_.isPlainObject(route) && METHODS.includes(route.method)) {
 		if (verbose) console.log('[@owservable/fastify-auto-routes] -> Initializing route', route.url);
